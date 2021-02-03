d709515458bbc2f70726b8ed79eda6273503dd7c
hossam.alissa <hossam.alissa@yahoo.com>
2021-02-03 06:14:24 +0200

    ADD GridView in RS

--- lib/screens/restaurant_screen.dart
+++ lib/screens/restaurant_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/models/food.dart';
 import 'package:flutter_food_delivery_ui/models/restaurant.dart';
 import 'package:flutter_food_delivery_ui/widgets/rating_stars.dart';
 
@@ -12,6 +13,93 @@ class RestaurantScreen extends StatefulWidget {
 }
 
 class _RestaurantScreenState extends State<RestaurantScreen> {
+  _buildMenuItem(Food menuItem) {
+    return Padding(
+      padding: EdgeInsets.all(7.0),
+      child: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              height: 175.0,
+              width: 175.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(menuItem.imageUrl),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+            ),
+            Container(
+              height: 175.0,
+              width: 175.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.topLeft,
+                    colors: [
+                      Colors.black.withOpacity(0.3),
+                      Colors.black87.withOpacity(0.3),
+                      Colors.black54.withOpacity(0.3),
+                      Colors.black38.withOpacity(0.3),
+                    ],
+                    stops: [
+                      0.1,
+                      0.4,
+                      0.6,
+                      0.9
+                    ]),
+              ),
+            ),
+            Positioned(
+              bottom: 65.0,
+              child: Column(
+                children: [
+                  Text(
+                    menuItem.name,
+                    style: TextStyle(
+                        fontSize: 24.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.2),
+                  ),
+                  Text(
+                    '\$${menuItem.price.toString()}',
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.2),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 10.0,
+              right: 10.0,
+              child: Container(
+                height: 45.0,
+                width: 45.0,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.add),
+                  iconSize: 30.0,
+                  color: Colors.white,
+                  onPressed: (){},
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,10 +111,7 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
                 tag: widget.restaurant.imageUrl,
                 child: Image(
                   height: 220.0,
-                  width: MediaQuery
-                      .of(context)
-                      .size
-                      .width,
+                  width: MediaQuery.of(context).size.width,
                   image: AssetImage(widget.restaurant.imageUrl),
                   fit: BoxFit.cover,
                 ),
@@ -44,9 +129,7 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
                     ),
                     IconButton(
                       icon: Icon(Icons.favorite_border),
-                      color: Theme
-                          .of(context)
-                          .primaryColor,
+                      color: Theme.of(context).primaryColor,
                       iconSize: 30.0,
                       onPressed: () {},
                     ),
@@ -88,21 +171,54 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-            FlatButton(
-              onPressed: () {},
-              padding: EdgeInsets.symmetric(horizontal: 30.0),
-              color: Theme.of(context).primaryColor,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-              child:Text("Reviews",style: TextStyle(color: Colors.white,fontSize: 20.0),),
+              FlatButton(
+                onPressed: () {},
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                color: Theme.of(context).primaryColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: Text(
+                  "Reviews",
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {},
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                color: Theme.of(context).primaryColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: Text(
+                  "Contact",
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10.0),
+          Center(
+            child: Text(
+              "Menu",
+              style: TextStyle(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1.2),
             ),
-            FlatButton(
-              onPressed: () {},
-              padding: EdgeInsets.symmetric(horizontal: 30.0),
-              color: Theme.of(context).primaryColor,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-              child:Text("Contact",style: TextStyle(color: Colors.white,fontSize: 20.0),),
+          ),
+          SizedBox(height: 10.0),
+          Expanded(
+            child: GridView.count(
+              padding: EdgeInsets.all(10.0),
+              crossAxisCount: 2,
+              children: List.generate(
+                widget.restaurant.menu.length,
+                (index) {
+                  Food food = widget.restaurant.menu[index];
+                  return _buildMenuItem(food);
+                },
+              ),
             ),
-          ],)
+          ),
         ],
       ),
     );
